88bd65dda7d4babe7066279988fb00428b248e93
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-07 19:37:22 +0200

    changed: from table view  to card view

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,31 +6,25 @@
 
 @section('content')
   <main>
-    <div class="pg-table-wrapper h-100 d-flex justify-content-center align-items-center overflow-x-scroll">
+    <div class="pg-table-wrapper h-100 d-flex justify-content-center align-items-center py-5">
       <div class="container">
-        <h2 class="mb-4">Lista dei film!</h2>
-        <table class="table align-middle">
-          <thead>
-            <tr>
-              <th scope="col">#ID</th>
-              <th scope="col">Titolo</th>
-              <th scope="col">Lingua</th>
-              <th scope="col">Data di uscita</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($movies as $movie)
-              <tr class="py-2">
-                <th scope="row">{{$movie->id}}</th>
-                <td>{{$movie->title}}</td>
-                <td>{{$movie->nationality}}</td>
-                <td>{{$movie->date}}</td>
-                <td><a href="{{route('film_info', ['id' => $movie->id])}}"><button type="button" class="btn btn-primary">Dettagli</button></a></td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
+        <h2 class="text-center">Lista dei film!</h2>
+        <div class="row row-cols-5">
+          @foreach ($movies as $movie)
+            <div class="col mt-5">
+              <div class="card h-100">
+                <img src="{{$movie->src}}" class="card-img-top h-75 object-fit-cover" alt="">
+                <div class="card-body d-flex flex-column justify-content-between">
+                  <h5 class="card-title pg-title mb-3">{{$movie->title}}</h5>
+
+                  <div class="pg-button-container text-center mt-5">
+                    <a href="{{route('film_info', ['id' => $movie->id])}}" class="btn btn-primary">Dettagli</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          @endforeach
+        </div>
       </div>
     </div>
   </main>
