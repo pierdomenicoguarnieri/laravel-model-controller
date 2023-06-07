962a102c5890b9d4c55fa9f5ce740ea7e9cfacf7
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-07 19:00:46 +0200

    fix: text alignement

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -7,7 +7,7 @@
 @section('content')
   <main>
     <div class="pg-contacts-wrapper h-100 d-flex flex-column justify-content-center align-items-center">
-      <div class="container">
+      <div class="container text-center">
         <h1 class="mb-4">Contatti</h1>
 
         <p>Nessun contatto disponibile, rispetta la mia privacy!</p>
